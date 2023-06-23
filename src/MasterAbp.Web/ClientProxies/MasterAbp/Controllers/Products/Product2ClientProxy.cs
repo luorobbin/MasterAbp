bd1eeb5030f8b9cf80721e0193bfbc55f5b9b727
luorobbin <896283381@qq.com>
2023-06-23 19:37:30 +0800

    增加静态代理

---
+++ src/MasterAbp.Web/ClientProxies/MasterAbp/Controllers/Products/Product2ClientProxy.cs
@@ -0,0 +1,7 @@
+// This file is part of Product2ClientProxy, you can customize it here
+// ReSharper disable once CheckNamespace
+namespace MasterAbp.Controllers.Products;
+
+public partial class Product2ClientProxy
+{
+}
